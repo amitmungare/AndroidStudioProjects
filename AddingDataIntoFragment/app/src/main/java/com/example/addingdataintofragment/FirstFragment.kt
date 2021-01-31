ae4fe77551fa426397e2518f778eea40a72b955a
Amit Mungare <amungare27@gmail.com>
2021-01-31 16:42:22 +0530

    fragment

---
+++ AddingDataIntoFragment/app/src/main/java/com/example/addingdataintofragment/FirstFragment.kt
@@ -0,0 +1,29 @@
+package com.example.addingdataintofragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_first.*
+
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_first, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+        btnfirst.setOnClickListener {
+            Toast.makeText(requireContext(),"hello ",Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+}
---
+++ AddingDataIntoFragment/app/src/main/java/com/example/addingdataintofragment/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.addingdataintofragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.flcontainer,FirstFragment())
+            .commit()
+
+    }
+}
