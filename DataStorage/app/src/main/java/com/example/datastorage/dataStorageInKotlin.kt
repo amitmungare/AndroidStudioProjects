d03c61b5142eebafd36f80e98fdc20782c415c88
Amit Mungare <amungare27@gmail.com>
2021-02-02 20:53:00 +0530

    data storage

---
+++ DataStorage/app/src/main/java/com/example/datastorage/dataStorageInKotlin.kt
@@ -0,0 +1,11 @@
+package com.example.datastorage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class dataStorageInKotlin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_data_storage_in_kotlin)
+    }
+}
